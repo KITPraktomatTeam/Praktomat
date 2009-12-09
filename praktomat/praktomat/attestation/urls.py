0ba27ffbebfb190906950fb2e572c5a5556b6b2f
kleinert <kleinert@81e6b769-b0d5-4359-94ed-cdb3f441ba95>
2009-12-09 11:34:02 +0000

    added
    git-svn-id: svn+ssh://ssh.info.uni-karlsruhe.de/ben/local/SVN/giffhorn/Django-Praktomat@86 81e6b769-b0d5-4359-94ed-cdb3f441ba95

--- praktomat/praktomat/attestation/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('praktomat.attestation.views',
-	url(r'^statistics$', 'statistics', name='statistics'),
-	url(r'^$', 'attestation_list', name='attestation_list'),
-	url(r'^$', 'attestation', name='attestation'),
-)
--- praktomat/praktomat/attestation/views.py
+++ praktomat/praktomat/attestation/views.py
@@ -8,10 +8,12 @@ from django.views.generic.list_detail import object_list, object_detail
 from praktomat.tasks.models import Task
 from praktomat.solutions.models import Solution, SolutionFile
 from praktomat.solutions.forms import SolutionFormSet
+from praktomat.attestation.models import Attestation, AnnotatedSolutionFile, RatingResult
+from praktomat.attestation.forms import AnnotatedFileFormSet, RatingResultFormSet, AttestationForm
 
 @login_required
 def statistics(request,task_id):
-	task = get_object_or_404(Task,pk=task_id)
+	task = get_object_or_404(Task, pk=task_id)
 	if not (request.user.groups.filter(name='Trainer').values('name') or request.user.is_superuser):
 		return render_to_response('error.html', context_instance=RequestContext(request))
 	solution_count = task.solution_set.filter(final=True).count()
@@ -27,11 +29,54 @@ def attestation_list(request, task_id):
 
 	
 @login_required
-def attestation(request,task_id):
-	task = get_object_or_404(Task,pk=task_id)
-	if not (request.user.groups.filter(name='Trainer').values('name') or request.user.is_superuser):
-		return render_to_response('error.html', context_instance=RequestContext(request))
-	solution_count = task.solution_set.filter(final=True).count()
-	from django.contrib.auth.models import Group
-	user_count = Group.objects.get(name='User').user_set.count()
-	return render_to_response("solutions/attestation.html", {'task':task, 'solution_count': solution_count,'user_count': user_count}, context_instance=RequestContext(request))
+def new_attestation(request, solution_id):
+	#if not (request.user.groups.filter(name='Trainer').values('name') or request.user.is_superuser):
+	#	return render_to_response('error.html', context_instance=RequestContext(request))
+	solution = get_object_or_404(Solution, pk=solution_id)
+	# If there already is an attestation by this user redirect to edit page
+	#if solution.attestation_set.count() > 0:
+	#	return HttpResponseRedirect(reverse('edit_attestation', args=[attest.id]))
+		
+	attest = Attestation(solution = solution, author = request.user)
+	attest.save()
+	for solutionFile in  solution.solutionfile_set.all():
+		annotatedFile = AnnotatedSolutionFile(attestation = attest, solution_file=solutionFile, content=solutionFile.content())
+		annotatedFile.save()
+	for rating in solution.task.rating_set.all():
+		ratingResult = RatingResult(attestation = attest, aspect=rating.aspect)
+		ratingResult.save()
+	return HttpResponseRedirect(reverse('edit_attestation', args=[attest.id]))
+	
+		
+		
+def edit_attestation(request, attestation_id):
+	#if not (request.user.groups.filter(name='Trainer').values('name') or request.user.is_superuser):
+	#	return render_to_response('error.html', context_instance=RequestContext(request))
+	attest = get_object_or_404(Attestation, pk=attestation_id)
+	solution = attest.solution
+
+	if request.method == "POST":
+		attestForm = AttestationForm(request.POST, instance=attest, prefix='attest')
+		attestFileFormSet = AnnotatedFileFormSet(request.POST, instance=attest, prefix='attestfiles')
+		ratingResultFormSet = RatingResultFormSet(request.POST, instance=attest, prefix='ratingresult')
+		if attestForm.is_valid() and attestFileFormSet.is_valid() and ratingResultFormSet.is_valid():
+			attestForm.save()
+			attestFileFormSet.save()
+			ratingResultFormSet.save()
+			return HttpResponseRedirect(reverse('edit_attestation', args=[attestation_id]))
+	else:
+		attestForm = AttestationForm(instance=attest, prefix='attest')
+		attestFileFormSet = AnnotatedFileFormSet(instance=attest, prefix='attestfiles')
+		ratingResultFormSet = RatingResultFormSet(instance=attest, prefix='ratingresult')
+	return render_to_response("attestation/attestation_edit.html", {"attestForm": attestForm, "attestFileFormSet": attestFileFormSet, "ratingResultFormSet":ratingResultFormSet, "solution": solution},	context_instance=RequestContext(request))
+
+
+
+
+
+
+
+
+
+
+
