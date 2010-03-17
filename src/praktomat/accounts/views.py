22a218032334df2c2003ce5203911725eb423373
kleinert <kleinert@81e6b769-b0d5-4359-94ed-cdb3f441ba95>
2010-03-17 22:36:48 +0000

    update to django 1.2-beta-1. updated views for Cross Site Request Forgery protection
    git-svn-id: svn+ssh://ssh.info.uni-karlsruhe.de/ben/local/SVN/giffhorn/Django-Praktomat@131 81e6b769-b0d5-4359-94ed-cdb3f441ba95

--- src/praktomat/accounts/views.py
+++ src/praktomat/accounts/views.py
@@ -8,13 +8,14 @@ from praktomat.accounts.models import UserProfile
 
 def register(request):
 	if request.method == 'POST':
-	        form = MyRegistrationForm(request.POST)
-	        if form.is_valid():
-	            form.save()
-	            return HttpResponseRedirect(reverse('registration_complete'))
+		form = MyRegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('registration_complete'))
 	else:
-	        form = MyRegistrationForm()
-	return render_to_response('registration/registration_form.html',{'form':form}, context_instance=RequestContext(request))
+		form = MyRegistrationForm()
+	return render_to_response('registration/registration_form.html', {'form':form}, context_instance=RequestContext(request))
+
 
 def activate(request, activation_key):
 	account = UserProfile.activate_user(activation_key)
