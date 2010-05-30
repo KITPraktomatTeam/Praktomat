255b4753aaae40acdfbf76abc2e3d8d656d458bd
Daniel Kleinert <herr.kleinert@googlemail.com>
2010-05-30 22:06:32 +0200

    Site name will now be displayed correctly.

--- src/praktomat/settings.py
+++ src/praktomat/settings.py
@@ -27,6 +27,15 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.load_template_source',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "praktomat.context_processors.current_site.current_site",
+    "django.contrib.auth.context_processors.auth",
+	"django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.contrib.messages.context_processors.messages"
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
