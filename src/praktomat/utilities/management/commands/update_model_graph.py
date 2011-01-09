4c135169fb2987f7d5305c3be4421871eefbef23
Daniel Kleinert <herr.kleinert@googlemail.com>
2011-01-09 17:42:09 +0100

    added command update_model_graph wich exports vector images of all models for documentation purporses

---
+++ src/praktomat/utilities/management/commands/update_model_graph.py
@@ -0,0 +1,23 @@
+from django.core import management
+from django.core.management.base import NoArgsCommand, CommandError
+from django.conf import settings
+from django.db import models
+
+from os.path import join, dirname as dir
+import shutil
+import tempfile
+
+class Command(NoArgsCommand):
+	help = 'Updates the vector images in documentation/modelgraph. Requires instalation of graphviz and pygraphviz.'
+
+	def handle_noargs(self, **options):
+			export_folder = join(dir(dir(dir(dir(dir(dir(__file__)))))),"documentation","model_graph")
+			management.call_command('graph_models', 'tasks', outputfile=join(export_folder,'tasks.svg'))
+			management.call_command('graph_models', 'solutions', outputfile=join(export_folder,'solutions.svg'))
+			management.call_command('graph_models', 'attestation', outputfile=join(export_folder,'attestation.svg'))
+			management.call_command('graph_models', 'accounts', outputfile=join(export_folder,'accounts.svg'))
+			management.call_command('graph_models', 'checker', outputfile=join(export_folder,'checker.svg'))
+			management.call_command('graph_models', 'tasks', 'solutions', 'attestation', 'accounts', group_models = True, disable_fields = True, outputfile=join(export_folder,'overview.svg'))
+			management.call_command('graph_models', all_applications = True, group_models = True, outputfile=join(export_folder,'extended_overview.svg'))
+			print 'Successfully updated model graph.'
+		
