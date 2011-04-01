d12b12551358079b61a28fd53120bd32c0e8c9d2
Daniel Kleinert <danielkleinert@Dagobert.local>
2011-04-01 16:22:21 +0200

    Function to distribute users to tutorials. Closes #51

--- src/praktomat/accounts/admin.py
+++ src/praktomat/accounts/admin.py
@@ -1,7 +1,9 @@
+from random import randint
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from django.contrib.auth.models import User as UserBase
 from django.contrib.auth.admin import UserAdmin as UserBaseAdmin
+from django.db.models import Count
 from praktomat.accounts.models import User, Tutorial
 from praktomat.accounts.templatetags.in_group import in_group
 from praktomat.accounts.forms import UserCreationForm, UserChangeForm
@@ -13,6 +15,7 @@ class UserAdmin(UserBaseAdmin):
 	# add active status
 	list_display = ('username', 'first_name', 'last_name', 'mat_number', 'tutorial', 'is_active', 'is_staff', 'is_superuser', 'is_trainer', 'is_tutor', 'email' )
 	list_filter = ('groups', 'tutorial', 'is_staff', 'is_superuser', 'is_active')
+	actions = ['distribute_to_tutorials']
 	readonly_fields = ('last_login','date_joined')
 	# exclude user_permissions
 	fieldsets = (
@@ -33,6 +36,21 @@ class UserAdmin(UserBaseAdmin):
 	def is_tutor(self, user):
 		return in_group(user,"Tutor")
 	is_tutor.boolean = True
+
+	def distribute_to_tutorials(self, request, queryset):
+		""" Distribute selectet users evenly to all tutorials """
+		users = list(queryset)
+		for user in users:
+			# remove tutorial from users so we can find the tutorial with the least amount of users
+			user.tutorial = None
+			user.save()
+		while(users):
+			# get random user an put him in the least populated tutorial
+			user = users.pop(randint(0,len(users)-1))
+			tutorial = Tutorial.objects.annotate(Count('user')).order_by('user__count')[0]
+			user.tutorial = tutorial
+			user.save()
+		self.message_user(request, "All users were successfully distributed.")
 
 admin.site.unregister(UserBase) 
 admin.site.register(User, UserAdmin)
