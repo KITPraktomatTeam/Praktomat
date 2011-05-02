8ceb5cc406e52f4a703882f74e16fc8db1a19e3b
FloKe <martin.hecker@kit.edu>
2011-05-02 15:16:07 +0200

    Hack: Windows-Codepages sind immer West-European

--- src/praktomat/utilities/encoding.py
+++ src/praktomat/utilities/encoding.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import chardet
+import re
 	
 def get_unicode(bytestring):
 	""" Returns guessed unicode representation of file content. """
-	return bytestring.decode(chardet.detect(bytestring)["encoding"])
+	return bytestring.decode(re.sub(r"ISO-8859-[0-9]","ISO-8859-1",chardet.detect(bytestring)["encoding"]))
 
 def get_utf8(unicodestring):
 	return unicodestring.encode("utf-8")
