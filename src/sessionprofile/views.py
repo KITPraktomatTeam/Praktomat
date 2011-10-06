3812f3dc531db683f7cc2ff3f4e51576792f70b8
praktomat <martin.hecker@kit.edu>
2011-10-06 14:26:41 +0200

    Use relative url for embedded Forum

--- src/sessionprofile/views.py
+++ src/sessionprofile/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
+from django.conf import settings
+
 
 def phpbb(request):
-	return render_to_response('sessionprofile/phpbb.html',{'phpbburl': 'https://praktomat.info.uni-karlsruhe.de/praktomat_2011_WS/forum/',},context_instance=RequestContext(request))
-#	return HttpResponse(t.render(c))
-#	return HttpResponse("Hello, world. You're at the poll index.")
+	return render_to_response('sessionprofile/phpbb.html',{'phpbburl': settings.BASE_URL+'forum/',},context_instance=RequestContext(request))
