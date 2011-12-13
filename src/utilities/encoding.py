19a31b8d73d20250264cbd232db7b38faca5bf0e
praktomat <martin.hecker@kit.edu>
2011-12-13 15:47:37 +0100

    Handle Upload of emmpty Files

--- src/utilities/encoding.py
+++ src/utilities/encoding.py
@@ -4,8 +4,11 @@ import chardet
 import re
 	
 def get_unicode(bytestring):
-	""" Returns guessed unicode representation of file content. """
-	return bytestring.decode(re.sub(r"ISO-8859-[0-9]","ISO-8859-1",chardet.detect(bytestring)["encoding"]))
+	if bytestring:
+		""" Returns guessed unicode representation of file content. """
+		return bytestring.decode(re.sub(r"ISO-8859-[0-9]","ISO-8859-1",chardet.detect(bytestring)["encoding"]))
+	else:
+		return u''
 
 def get_utf8(unicodestring):
 	return unicodestring.encode("utf-8")
