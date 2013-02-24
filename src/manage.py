a9e358ed7237400f6ad027282a1bf10dc7f6a981
Martin Morgenstern <rm.morgenstern@gmail.com>
2013-02-24 13:40:36 +0100

    Replace manage.py with a working version

--- src/manage.py
+++ src/manage.py
@@ -1,45 +1,9 @@
 #!/usr/bin/env python
-from django.core.management import execute_manager
-try:
-    import settings # Assumed to be in the same directory.
-except ImportError:
-    import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the  directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-    sys.exit(1)
+import os
+import sys
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+from django.core.management import execute_from_command_line
 if __name__ == "__main__":
-
-
-    import sys
-
-    command = ''
-    if len(sys.argv) > 1:
-        command = sys.argv[1]
-
-    if settings.DEBUG and (command == "runserver" or command == "testserver"):
-
-        # Make pydev debugger works for auto reload.
-        try:
-            import pydevd
-        except ImportError:
-            sys.stderr.write("Error: " +
-               "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
-            sys.exit(1)
-
-        from django.utils import autoreload
-        m = autoreload.main
-        def main(main_func, args=None, kwargs=None):
-            import os
-            if os.environ.get("RUN_MAIN") == "true":
-                def pydevdDecorator(func):
-                    def wrap(*args, **kws):
-                        pydevd.settrace(suspend=False, port=5678)
-                        return func(*args, **kws)
-                    return wrap
-                main_func = pydevdDecorator(main_func)
-
-            return m(main_func, args, kwargs)
-
-        autoreload.main = main
-
-    execute_manager(settings)
+    execute_from_command_line(sys.argv)
