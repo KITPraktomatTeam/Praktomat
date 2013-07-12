f28404b6cb054fd54eb88edb1282def3929a1f07
Joachim Breitner <mail@joachim-breitner.de>
2013-07-12 18:08:45 +0200

    Use example private key

--- src/settings_test.py
+++ src/settings_test.py
@@ -1,5 +1,10 @@
+from os.path import dirname, join
+
 from settings import * 
 
 # This failed here (ulimit -f in checker/scripts/execute showed 0)
 TEST_MAXFILESIZE = None
+
 UPLOAD_ROOT = "/tmp/upload"
+
+PRIVATE_KEY = join(dirname(dirname(__file__)), 'examples', 'certificates', 'privkey.pem')
