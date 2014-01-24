4b9cf1692654b878ffc43b62910ed87cf0dea586
Joachim Breitner <breitner@kit.edu>
2014-01-24 18:22:51 +0100

    Typo (grml, I want a type system)

--- src/checker/checker/ScriptChecker.py
+++ src/checker/checker/ScriptChecker.py
@@ -56,7 +56,7 @@ class ScriptChecker(Checker):
 
 		if self.remove:
 			output = re.sub(self.remove, "", output)
-		if not self.returns_html or truncated or timeout:
+		if not self.returns_html or truncated or timed_out:
 			output = '<pre>' + escape(output) + '</pre>'
 
 		result.set_log(output,timed_out=timed_out,truncated=truncated)
