5f5cd5926a12ad0c8be8b3d81d6f01930b974db4
Joachim Breitner <breitner@kit.edu>
2014-05-15 16:13:31 +0200

    Make utilities.encoding.get_unicode more robust
    This somewhat fixes #166

--- src/utilities/encoding.py
+++ src/utilities/encoding.py
@@ -4,7 +4,6 @@ import chardet
 import re
 	
 def get_unicode(bytestring):
-	default_charsets = ["ISO-8859-1","utf-8"]
 	if bytestring:
 		""" Returns guessed unicode representation of file content. """
 		if isinstance(bytestring,unicode):
@@ -19,12 +18,13 @@ def get_unicode(bytestring):
 			charset = re.sub(r"windows-125[01235]","ISO-8859-1",charset)
 
 		for chset in ["utf-8", charset, "ISO-8859-1"]:
-			try:
-				return bytestring.decode(chset)
-			except UnicodeDecodeError:
-				pass
-			except LookupError:
-				pass
+			if chset:
+				try:
+					return bytestring.decode(chset)
+				except UnicodeDecodeError:
+					pass
+				except LookupError:
+					pass
 		raise UnicodeDecodeError("Unable to detect proper characterset")
 	else:
 		return u''
