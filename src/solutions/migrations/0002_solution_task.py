93869de8e698d7c8bcbd25cf2622318cbd14f220
Joachim Breitner <mail@joachim-breitner.de>
2015-10-06 14:13:40 +0200

    Reset migrations, now that django supports them natively
    and South is no longer used.

---
+++ src/solutions/migrations/0002_solution_task.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0001_initial'),
+        ('solutions', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='solution',
+            name='task',
+            field=models.ForeignKey(to='tasks.Task'),
+        ),
+    ]
