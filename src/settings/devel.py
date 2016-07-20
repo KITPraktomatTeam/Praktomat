06ac78fa1151a38187e871c48e2180b38bdf4661
Robert Hartmann (FB02, H-BRS) <Robert.Hartmann@h-brs.de>
2016-07-20 09:56:54 +0200

    individualised Settings (devel.py + local.py) , remove old backup settingsfile

--- src/settings/devel.py
+++ src/settings/devel.py
@@ -2,47 +2,107 @@
 
 from os.path import join, dirname
 
+PRAKTOMAT_PATH = dirname(dirname(dirname(__file__)))
+
 # The name that will be displayed on top of the page and in emails.
-SITE_NAME = 'Praktomattest - devel'
+SITE_NAME = 'TestVM - Praktomat - devel'
 
 # Identifie this Praktomat among multiple installation on one webserver
-PRAKTOMAT_ID = 'default' 
+PRAKTOMAT_ID = 'devel' 
 
 # The URL where this site is reachable. 'http://localhost:8000/' in case of the
 # developmentserver.
 #BASE_HOST = 'http://praktomattest.inf.h-brs.de:8910'
 BASE_HOST = 'http://localhost:8000'
-BASE_PATH = '/'
+BASE_PATH = '/'+ PRAKTOMAT_ID + '/'
 
 # URL to use when referring to static files.
 STATIC_URL = BASE_PATH + 'static/'
+STATIC_ROOT = join(dirname(PRAKTOMAT_PATH), "static")
 
 # Absolute path to the directory that shall hold all uploaded files as well as
 # files created at runtime.
 
 # Example: "/home/media/media.lawrence.com/"
-UPLOAD_ROOT = join(dirname(dirname(dirname(__file__))),'data')
+# UPLOAD_ROOT = join(dirname(PRAKTOMAT_PATH), "PraktomatSupport/")
+# UPLOAD_ROOT = "/home/praktomat/inst/" + PRAKTOMAT_ID + "/work/"
+#UPLOAD_ROOT = join(dirname(dirname(dirname(__file__))),'data')
+UPLOAD_ROOT = join(dirname(PRAKTOMAT_PATH), "debug-work/")
+
+
+#SANDBOX_DIR = join('/srv/praktomat/sandbox/', PRAKTOMAT_ID)
+#SANDBOX_DIR = join(UPLOAD_ROOT, "SolutionSandbox/")
+#SANDBOX_DIR now defined in settings/defaults.py
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 25
-DEFAULT_FROM_EMAIL = "praktomattest@inf.h-brs.de"
+DEFAULT_FROM_EMAIL = "praktomat_rhVM@inf.h-brs.de"
 
 DEBUG = True
+
+# Old Comments ... I like them to keep
+#Configuration for PostGreSQL
+#DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = 'Praktomat'   # Or path to database file if using sqlite3.
+#DATABASE_USER = 'postgres'    # Not used with sqlite3.
+#DATABASE_PASSWORD = 'postgres'# Not used with sqlite3.
+#DATABASE_HOST = 'localhost'   # Set to empty string for localhost. Not used with sqlite3.
+#DATABASE_PORT = '5432'        # Set to empty string for default. Not used with sqlite3.
+
+#Configuration for SQLite
+#DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = UPLOAD_ROOT+'/Database'   # Or path to database file if using sqlite3.
+#DATABASE_USER = ''             # Not used with sqlite3.
+#DATABASE_PASSWORD = ''         # Not used with sqlite3.
+#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
 
 
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME':   UPLOAD_ROOT+'/Database',
+            'NAME':   UPLOAD_ROOT+'/Database'+PRAKTOMAT_ID,
     }
 }
 
+#overwrite to use the same settings from local.py
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME':   'praktomat_'+PRAKTOMAT_ID,
+            'USER':   'praktomat',
+            'PASSWORD':   'praktomat',
+    }
+}
+# on linux command line  create database and databaseuser 
+#sudo -u postgres -P <db_user>
+#sudo -u postgres -P praktomat
+#
+#sudo -u postgres createdb -O <db_user> <db_name>
+#sudo -u postgres createdb -O praktomat praktomat_devel
+
+    
+
+
+# SECRET_KEY gets generated via defaults.py
 
 PRIVATE_KEY = join(dirname(dirname(dirname(__file__))), 'examples', 'certificates', 'privkey.pem')
 
+#overwrite to use the same settings from local.py
+PRIVATE_KEY = None
+
+# Enable Shibboleth:
 SHIB_ENABLED = False
+
+# Set this to False to disable registration via the website, e.g. when Single Sign On is used
 REGISTRATION_POSSIBLE = False
+
+# If you use shibboleth identitiy provider, please have a look into defaults.py 
+# to see how to overwrite SHIB_ATTRIBUTE_MAP , SHIB_USERNAME , SHIB_PROVIDER 
+
 
 ACCOUNT_CHANGE_POSSIBLE = False
 DUMMY_MAT_NUMBERS = True
@@ -57,11 +117,42 @@ LDAP_URI="ldap://ldap.inf.fh-brs.de"
 LDAP_BASE="dc=fh-bonn-rhein-sieg,dc=de"
 
 
-# Use a dedicated user to test submissions
-USEPRAKTOMATTESTER = False
 
+# Use a dedicated user "tester" to test submissions
+USEPRAKTOMATTESTER = True
+
+# It is recomendet to use DOCKER and not a tester account
+# for using Docker from https://github.com/nomeata/safe-docker
 # Use docker to test submission
-USESAFEDOCKER = True
+USESAFEDOCKER = False
+
+# Linux User "tester" and Usergroup "praktomat"
+# Enable to run all scripts (checker) as the unix user 'tester'. 
+# Therefore put 'tester' as well as the Apache user '_www' (and your development user account) 
+# into a new group called "praktomat". 
+
+# Also edit your sudoers file with "sudo visudo -f /etc/sudoers.d/praktomat-tester". 
+# Add the following lines to the end of the file to allow the execution of 
+# commands with the user 'tester' without requiring a password:
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+
+# Add the following lines to the end of the file 
+# to allow user Praktomat the execution of scriptfile  safe-docker  without requiring a password:
+# "praktomat	ALL= NOPASSWD: /usr/local/bin/safe-docker"
+
+# If you want to switch between "testuser" and "Docker" 
+# use "sudo visudo -f /etc/sudoers.d/praktomat-tester"
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+# "praktomat 	ALL=(tester) NOPASSWD: ALL, NOPASSWD: /usr/local/bin/safe-docker"
+#
+# be shure that you change file permission 
+# sudo chown praktomat:tester praktomat/src/checker/scripts/java
+# sudo chown praktomat:tester praktomat/src/checker/scripts/javac
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/java
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/javac 
+
 
 
 
--- src/settings/local.py
+++ src/settings/local.py
@@ -48,25 +48,26 @@ PRAKTOMAT_PATH = dirname(dirname(dirname(__file__)))
 
 # Hard overwriting Praktomat_id 
 # Identifie this Praktomat among multiple installation on one webserver
-PRAKTOMAT_ID = '2016s' 
+PRAKTOMAT_ID = '2016w' 
+
 
 # The name that will be displayed on top of the page and in emails.
-SITE_NAME = 'Praktomat Sommersemester 2016 (AlgoDat)'
+SITE_NAME = 'TestVM - Praktomat - local'
 
 
 
 # The URL where this site is reachable. 'http://localhost:8000/' in case of the
 # developmentserver.
 #BASE_HOST = 'https://praktomat.cs.kit.edu'
-BASE_HOST = 'https://praktomattest.inf.h-brs.de/'
+BASE_HOST = 'https://localhost/'
 BASE_PATH = '/' + PRAKTOMAT_ID + '/'
 
 #ALLOWED_HOSTS = [ 'praktomat.cs.kit.edu', ]
-ALLOWED_HOSTS = [ 'praktomattest.inf.h-brs.de', ]
+#ALLOWED_HOSTS = [ 'praktomattest.inf.h-brs.de', ]
+ALLOWED_HOSTS = [ 'localhost', ]
 
 # URL to use when referring to static files.
 STATIC_URL = BASE_PATH + 'static/'
-
 STATIC_ROOT = join(dirname(PRAKTOMAT_PATH), "static")
 
 #if "cram" in PRAKTOMAT_ID:
@@ -82,7 +83,8 @@ UPLOAD_ROOT = join(dirname(PRAKTOMAT_PATH), "work/")
 
 
 #SANDBOX_DIR = join('/srv/praktomat/sandbox/', PRAKTOMAT_ID)
-SANDBOX_DIR = join(UPLOAD_ROOT, "SolutionSandbox/")
+#SANDBOX_DIR = join(UPLOAD_ROOT, "SolutionSandbox/")
+#SANDBOX_DIR now defined in settings/defaults.py
 
 ADMINS = [
   #('Praktomat', 'praktomat@ipd.info.uni-karlsruhe.de')
@@ -108,12 +110,50 @@ DEFAULT_FROM_EMAIL = "praktomattest@inf.h-brs.de"
 #DEBUG = MIRROR
 DEBUG = False
 
+
+
+
+# Old Comments ... I like them to keep
+#Configuration for PostGreSQL
+#DATABASE_ENGINE = 'postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = 'Praktomat'   # Or path to database file if using sqlite3.
+#DATABASE_USER = 'postgres'    # Not used with sqlite3.
+#DATABASE_PASSWORD = 'postgres'# Not used with sqlite3.
+#DATABASE_HOST = 'localhost'   # Set to empty string for localhost. Not used with sqlite3.
+#DATABASE_PORT = '5432'        # Set to empty string for default. Not used with sqlite3.
+
+#Configuration for SQLite
+#DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+#DATABASE_NAME = UPLOAD_ROOT+'/Database'   # Or path to database file if using sqlite3.
+#DATABASE_USER = ''             # Not used with sqlite3.
+#DATABASE_PASSWORD = ''         # Not used with sqlite3.
+#DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+#DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+DATABASES = {
+    'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME':   UPLOAD_ROOT+'/Database'+PRAKTOMAT_ID,
+    }
+}
+
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME':   PRAKTOMAT_ID,
+            'NAME':   'praktomat_'+PRAKTOMAT_ID,
+            'USER':   'praktomat',
+            'PASSWORD':   'praktomat',
     }
 }
+# on linux command line  create database and databaseuser 
+#sudo -u postgres -P <db_user>
+#sudo -u postgres -P praktomat
+#
+#sudo -u postgres createdb -O <db_user> <db_name>
+#sudo -u postgres createdb -O praktomat praktomat_2016w
+
+
+# SECRET_KEY gets generated via defaults.py
 
 # Private key used to sign uploded solution files in submission confirmation email
 #PRIVATE_KEY = '/srv/praktomat/mailsign/signer_key.pem'
@@ -121,7 +161,12 @@ PRIVATE_KEY = None # '/home/praktomat/certificates/privkey.pem'
 
 # Enable Shibboleth:
 SHIB_ENABLED = False
+
+# Set this to False to disable registration via the website, e.g. when Single Sign On is used
 REGISTRATION_POSSIBLE = False
+
+# If you use shibboleth identitiy provider, please have a look into defaults.py 
+# to see how to overwrite SHIB_ATTRIBUTE_MAP , SHIB_USERNAME , SHIB_PROVIDER 
 
 ACCOUNT_CHANGE_POSSIBLE = False
 DUMMY_MAT_NUMBERS = True
@@ -139,11 +184,43 @@ LDAP_BASE="dc=fh-bonn-rhein-sieg,dc=de"
 
 
 
-# Use a dedicated user to test submissions
-USEPRAKTOMATTESTER = False
+# Use a dedicated user "tester" to test submissions
+USEPRAKTOMATTESTER = True
 
+# It is recomendet to use DOCKER and not a tester account
+# for using Docker from https://github.com/nomeata/safe-docker
 # Use docker to test submission
-USESAFEDOCKER = True
+USESAFEDOCKER = False
+
+# Linux User "tester" and Usergroup "praktomat"
+# Enable to run all scripts (checker) as the unix user 'tester'. 
+# Therefore put 'tester' as well as the Apache user '_www' (and your development user account) 
+# into a new group called "praktomat". 
+
+# Also edit your sudoers file with "sudo visudo -f /etc/sudoers.d/praktomat-tester". 
+# Add the following lines to the end of the file to allow the execution of 
+# commands with the user 'tester' without requiring a password:
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+
+# Add the following lines to the end of the file 
+# to allow user Praktomat the execution of scriptfile  safe-docker  without requiring a password:
+# "praktomat	ALL= NOPASSWD: /usr/local/bin/safe-docker"
+
+# If you want to switch between "testuser" and "Docker" 
+# use "sudo visudo -f /etc/sudoers.d/praktomat-tester"
+# "_www		ALL=(tester) NOPASSWD: ALL"
+# "developer	ALL=(tester) NOPASSWD: ALL"
+# "praktomat 	ALL=(tester) NOPASSWD: ALL, NOPASSWD: /usr/local/bin/safe-docker"
+#
+# be shure that you change file permission 
+# sudo chown praktomat:tester praktomat/src/checker/scripts/java
+# sudo chown praktomat:tester praktomat/src/checker/scripts/javac
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/java
+# sudo chmod u+x,g+x,o-x praktomat/src/checker/scripts/javac 
+
+
+
 
 # Various extra files and versions
 #CHECKSTYLEALLJAR = '/srv/praktomat/contrib/checkstyle-5.7-all.jar'
@@ -155,7 +232,6 @@ JAVA_LIBS = { 'junit3' : '/usr/share/java/junit.jar', 'junit4' : '/opt/praktomat
 # Our VM has 4 cores, so lets try to use them
 # HBRS only 2 
 NUMBER_OF_TASKS_TO_BE_CHECKED_IN_PARALLEL = 2
-
 
 # Finally load defaults for missing settings.
 import defaults
