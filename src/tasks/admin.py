65f453310dd8d4c24f8e4a194f137b160a95776e
Robert Hartmann (FB02, H-BRS) <Robert.Hartmann@h-brs.de>
2017-02-28 16:30:22 +0100

    Task-Admin: adding functions and changed run_all_checkers     - run_all_checkers_on_finals     - run_all_checkers_on_latest_only_failed     (introduced with AutoAttestChecker but not realy directly related to it)
    Signed-off-by: Robert Hartmann (FB02, H-BRS) <Robert.Hartmann@h-brs.de>

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -44,7 +44,7 @@ class TaskAdmin(admin.ModelAdmin):
 	date_hierarchy = 'publication_date'
 	save_on_top = True
 	inlines = [MediaInline] + CheckerInline.__subclasses__() + [ RatingAdminInline]
-	actions = ['export_tasks', 'run_all_checkers']
+	actions = ['export_tasks', 'run_all_checkers', 'run_all_checkers_on_finals', 'run_all_checkers_on_latest_only_failed']
 	
 	formfield_overrides = {
         models.TextField: {'widget': TinyMCE()},
@@ -65,16 +65,49 @@ class TaskAdmin(admin.ModelAdmin):
 		response = HttpResponse(Task.export_Tasks(queryset).read(), content_type="application/zip")
 		response['Content-Disposition'] = 'attachment; filename=TaskExport.zip'
 		return response
-		
+
 	
-	def run_all_checkers(self, request, queryset):
-		""" Rerun all checker including "not always" action """
+	def run_all_checkers_on_finals(self, request, queryset):
+		""" Rerun all checker on final solutions including "not always" action """
 		start = timer()
 		count = 0
 		for task in queryset:
 			count += task.check_all_final_solutions()
 		end = timer()
 		self.message_user(request, "%d final solutions were successfully checked (%d seconds elapsed)." % (count, end-start))
+
+
+	def run_all_checkers_on_latest_only_failed(self,request, queryset):
+		""" Rerun all checker on latest of only failed solutions including "not always" action """
+		from checker.basemodels import check_solution
+		from accounts.models import User
+		start = timer()
+		count = 0
+		for task in queryset:
+			solution_queryset = task.solution_set
+			final_solutions_queryset = solution_queryset.filter(final=True)
+			finalusers = list(set(final_solutions_queryset.values('author').values_list('author', flat=True)))
+			only_failed_solution_set = solution_queryset.exclude(author__in=finalusers)
+
+			nonfinalusers = list( set(User.objects.all().values('id').values_list('id',flat=True)) - set(finalusers))
+						
+			for user in User.objects.filter(id__in=nonfinalusers) :
+				users_only_failed_solution = only_failed_solution_set.filter(author=user).order_by('author','number') 
+				ucount = int(users_only_failed_solution.count())
+				if ucount :
+					latest_only_failed_solution=users_only_failed_solution.latest('number')
+					check_solution(latest_only_failed_solution,True)					
+					count += 1
+		end = timer()
+		self.message_user(request, "%d users with only failed solutions were checked (%d seconds elapsed)." % (count, end-start))
+
+
+
+	def run_all_checkers(self, request, queryset):
+		""" Rerun all checker including "not always" action for students with final or latest of only failed solutions """
+		self.run_all_checkers_on_finals(request, queryset)
+		self.run_all_checkers_on_latest_only_failed(request, queryset)
+
 
 	def get_urls(self):
 		""" Add URL to task import """
