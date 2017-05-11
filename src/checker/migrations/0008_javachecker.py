baabdee61621397802a04d3752787ea92085b4b9
Moritz Schubotz <github@ckurs.de>
2017-05-12 00:01:40 +0200

    Submit migration file as it was generated

--- src/checker/migrations/0008_javachecker.py
+++ src/checker/migrations/0008_javachecker.py
@@ -2,14 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import checker.basemodels
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tasks', '0002_task_jplag_up_to_date'),
-        ('checker', '0004_textchecker_and_scalabuilder'),
+        ('tasks', '0003_htmlinjector'),
+        ('checker', '0007_auto_20170429_2022'),
     ]
 
     operations = [
@@ -31,5 +30,15 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
-        )
+        ),
+        migrations.AlterField(
+            model_name='junitchecker',
+            name='ignore',
+            field=models.CharField(default=b'', help_text='space-seperated list of files to be ignored during compilation, i.e.: these files will not be compiled.', max_length=4096, blank=True),
+        ),
+        migrations.AlterField(
+            model_name='scriptchecker',
+            name='filename',
+            field=models.CharField(help_text='What the file will be named in the sandbox. If empty, we try to guess the right filename!', max_length=500, blank=True),
+        ),
     ]
