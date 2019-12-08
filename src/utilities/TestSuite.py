b70658ed080258dbff03f6dde43358001304a78c
Robert Hartmann (@Laptop, WSL) <Robert.Hartmann@h-brs.de>
2019-12-09 00:20:32 +0100

    src/utilities/TestSuite.py: UnicodeEncodeError : line 136, in create_test_data

--- src/utilities/TestSuite.py
+++ src/utilities/TestSuite.py
@@ -131,7 +131,16 @@ def create_test_data():
                 u'Tasks',
                 u'GGT',
                 u'solutions',
-                u'GgT.java'))
+                u'GgT.java')))
+    print("sys:defaultencoding: " , sys.getdefaultencoding()
+    print("sys:filesystemencoding: " ,sys.getfilesystemencoding(),
+    print("locale:preferredencoding: " locale.getpreferredencoding(),
+    print("locale:defaultlocale ",locale.getdefaultlocale()
+    print("locale:locale ",locale.getlocale()
+    print(type(tempF))
+    tempC=tempF.read()
+    print(type(tempC))
+    print(tempC)
     solution_file.file.save(
                 u'GgT.java',
                 tempF
