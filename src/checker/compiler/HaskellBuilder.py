7840eaceea90b48d9f70698245371e35068f9511
Robert Hartmann (FB02, H-BRS) <Robert.Hartmann@h-brs.de>
2021-04-09 20:59:47 +0200

    Add LANG and LANGUAGE to HaskellBuilder env

--- src/checker/compiler/HaskellBuilder.py
+++ src/checker/compiler/HaskellBuilder.py
@@ -48,7 +48,10 @@ class HaskellBuilder(Builder):
 
         filenames = [name for name in self.get_file_names(env)]
         args = [self.compiler()] + self.flags(env) + filenames + self.libs()
-        [output, _, _, _, _]  = execute_arglist(args, env.tmpdir(), self.environment())
+        environ = self.environment()
+        environ['LANG'] = settings.LANG
+        environ['LANGUAGE'] = settings.LANGUAGE
+        [output, _, _, _, _]  = execute_arglist(args, env.tmpdir(), environ)
 
         has_main = re.search(r"^Linking ([^ ]*) ...$", output, re.MULTILINE)
         if has_main: self._detected_main = has_main.group(1)
