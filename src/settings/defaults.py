dbf9256dcdff164b96bd2cfd900343d456cdc07e
Moritz Schubotz <wiki@physikerwelt.de>
2021-10-09 15:06:39 +0200

    Fix: Close curly

--- src/settings/defaults.py
+++ src/settings/defaults.py
@@ -267,7 +267,7 @@ def load_defaults(settings):
     d.DEJAGNU_RUNTEST = '/usr/bin/runtest'
     d.CHECKSTYLEALLJAR = '/home/praktomat/contrib/checkstyle-all-4.4.jar'
     d.JUNIT38='junit'
-    d.JAVA_LIBS = { 'jun' : '/usr/share/java/junit.jar', 'junit4' : '/usr/share/java/junit4.jar'
+    d.JAVA_LIBS = { 'jun' : '/usr/share/java/junit.jar', 'junit4' : '/usr/share/java/junit4.jar' }
     d.JAVA_CUSTOM_LIBS = PRAKTOMAT_ROOT + '/lib/java/*'
     d.JCFDUMP='jcf-dump'
 
