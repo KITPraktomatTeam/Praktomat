dca9d29e4a40b351d8f982cd705963d55515b546
Abdallah Abouelatta <74081035+Abdallah-Abouelatta@users.noreply.github.com>
2023-01-27 05:26:39 +0100

    comment modified
    A line that was previously too long has been broken into two separate lines for better readability.

--- src/solutions/views.py
+++ src/solutions/views.py
@@ -168,7 +168,8 @@ def solution_list(request, task_id, user_id=None):
                          message.send() # any PY2-PY3 problem in here ?
 
                 else: #we are sending unsigned email
-                    #one of the checks is: if the user has selected the checkbox on the 'Change Account' page to confirm their willingness to receive an email fo each file upload ('False' by default)
+                    #one of the checks is: if the user has selected the checkbox on the 'Change Account' page to confirm 
+                    #their willingness to receive an email fo each file upload ('False' by default)
                     if solution.author.email and User.upload_confirm_emails:
                          send_mail(_("%s submission confirmation") % settings.SITE_NAME, t.render(c), None, [solution.author.email])
 
